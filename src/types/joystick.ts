27aa269fd1675bcd3231eef8b50bd282f13c6d4a
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2025-01-20 14:42:09 -0300

    [wip] joystick on data-lake

--- src/types/joystick.ts
+++ src/types/joystick.ts
@@ -50,22 +50,34 @@ export class Joystick {
   }
 
   /**
-   * Returns current state of axes and buttons
+   * Returns current raw state of axes and buttons
+   * @returns {JoystickState}
+   */
+  get rawState(): JoystickState {
+    return {
+      buttons: this.gamepad.buttons.map((button) => button.value),
+      axes: this.gamepad.axes.map((axis) => axis),
+    }
+  }
+
+  /**
+   * Returns current state of axes and buttons.
+   * The state is mapped to the Cockpit standard if the joystick has a mapping.
+   * If the joystick has no mapping, it returns the raw state.
    * @returns {JoystickState}
    */
   get state(): JoystickState {
-    return {
-      buttons:
-        this.gamepadToCockpitMap?.buttons.map((idx) => {
-          if (idx === null || this.gamepad.buttons[idx] === undefined) return undefined
-          return this.gamepad.buttons[idx].value
-        }) || [],
-      axes:
-        this.gamepadToCockpitMap?.axes.map((idx) => {
-          if (idx === null || this.gamepad.axes[idx] === undefined) return undefined
-          return this.gamepad.axes[idx]
-        }) || [],
-    }
+    if (!this.gamepadToCockpitMap) return this.rawState
+
+    const buttons = this.gamepadToCockpitMap.buttons.map((corr) => {
+      return corr.indexInGamepadAPI === null ? undefined : this.gamepad.buttons[corr.indexInGamepadAPI].value
+    })
+
+    const axes = this.gamepadToCockpitMap.axes.map((corr) => {
+      return corr.indexInGamepadAPI === null ? undefined : this.gamepad.axes[corr.indexInGamepadAPI]
+    })
+
+    return { buttons, axes }
   }
 }
 
@@ -158,47 +170,29 @@ export interface JoystickProtocolActionsMapping {
   }
 }
 
-export type CockpitButton = null | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20 | 21 | 22 | 23 | 24 | 25 | 26 | 27 | 28 | 29 | 30 | 31 // eslint-disable-line
-export type CockpitAxis = null | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20 | 21 | 22 | 23 | 24 | 25 | 26 | 27 | 28 | 29 | 30 | 31 // eslint-disable-line
+export type GamepadToCockpitInputCorrespondency = {
+  /**
+   * The index of the input in the Gamepad API input array
+   */
+  indexInGamepadAPI: number | null
+  /**
+   * The id of the input in the Cockpit standard
+   */
+  idInCockpitStd: JoystickButton | JoystickAxis
+}
 
 /**
  * This interface defines the mapping for a specific controller from the Gamepad API to Cockpit's standard.
  */
 export interface GamepadToCockpitStdMapping {
   /**
-   *  Name to help identification of a mapping profile
-   */
-  name: string
-  /**
    * Correspondency from Gamepad API to Cockpit standard axes.
-   * Corresponds to which Axis in the Cockpit standard should the Nth axis be mapped to.
-   * E.g.: [2, 0, 1, 3] means axis 0 in the Gamepad API will provide data to axis 2 in Cockpit, while axis 1
-   * in the Gamepad API will provide data to axis 0 in Cockpit.
-   * Gamepad API / Cockpit API
-   *      0     ->     2
-   *      1     ->     0
-   *      2     ->     1
-   *      3     ->     undefined
-   *     ...    ->    ...
-   *
-   * You can notice that in the previous case axis 3 in the Gamepad API does is not used in the Cockpit API
    */
-  axes: CockpitAxis[]
+  axes: GamepadToCockpitInputCorrespondency[]
   /**
    * Correspondency from Gamepad API to Cockpit standard buttons.
-   * Corresponds to which button in the Cockpit standard should the Nth button be mapped to.
-   * E.g.: [3, 4, 15, 2, ...] means button 0 in the Gamepad API will provide data to button 3 in the Cockpit
-   * API, while button 1 in the Gamepad API will provide data to button 4 in the Cockpit API.
-   * Gamepad API / Cockpit API
-   *      0     ->     3
-   *      1     ->     4
-   *      2     ->     15
-   *      3     ->     undefined
-   *     ...    ->    ...
-   *
-   * You can notice that in the previous case button 3 in the Gamepad API does is not used in the Cockpit API
    */
-  buttons: CockpitButton[]
+  buttons: GamepadToCockpitInputCorrespondency[]
 }
 
 /**
