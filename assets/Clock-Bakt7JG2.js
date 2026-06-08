1e3e6c786a1e36569e345f6ef8554ef8dcc56c3c
rafaellehmkuhl <rafael.lehmkuhl93@gmail.com>
2026-06-08 17:08:09 +0000

    Deploying to gh-pages from @ rafaellehmkuhl/cockpit-public@51e1cf1659662e21674419eff9fb336eef5c54b3 🚀

---
+++ assets/Clock-Bakt7JG2.js
@@ -0,0 +1,1 @@
+import{d as s,bC as n,p as o,q as a,W as c,z as e,bH as m}from"./index-BW-zAWeG.js";const r={class:"flex items-center justify-center m-2 text-sm font-bold text-center text-white select-none min-w-[90px]"},f=s({__name:"Clock",setup(i){const t=n({interval:1e3});return(l,p)=>(a(),o("div",r,c(e(m)(e(t),"E LLL do HH:mm")),1))}});export{f as default};
